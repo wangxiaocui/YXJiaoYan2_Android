5c6b8e769b7e890d8c19b08817eb470a78ed64e3
huchao <huchao@yanxiu.com>
2018-10-10 11:50:18 +0800

    添加登录和验证码登录页

---
+++ module_signin/src/main/java/com/yanxiu/gphone/jiaoyan/module/signin/activity/LoginActivity.java
@@ -0,0 +1,124 @@
+package com.yanxiu.gphone.jiaoyan.module.signin.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.test.yanxiu.common_base.base.ui.JYBaseActivity;
+import com.test.yanxiu.common_base.route.RoutePathConfig;
+import com.test.yanxiu.common_base.route.RouteUtils;
+import com.yanxiu.gphone.jiaoyan.module.signin.R;
+import com.yanxiu.gphone.jiaoyan.module.signin.interfaces.LoginContract;
+import com.yanxiu.gphone.jiaoyan.module.signin.presenter.LoginPresenter;
+import com.yanxiu.lib.yx_basic_library.util.YXToastUtil;
+
+/**
+ * 登录页面
+ * Created by Hu Chao on 18/10/9.
+ */
+@Route(path = RoutePathConfig.SIGNIN_LOGIN_ACTIVITY)
+public class LoginActivity extends JYBaseActivity<LoginContract.IPresenter> implements LoginContract.IView {
+
+    protected EditText et_account;
+    protected EditText et_password;
+    protected TextView tv_get_code;
+    protected Button btn_login;
+    protected TextView tv_login_type;
+    protected TextView tv_register;
+
+
+    @Override
+    protected LoginContract.IPresenter initPresenterImpl() {
+        return new LoginPresenter(this);
+    }
+
+    @Override
+    public void initData(@Nullable Bundle bundle) {
+    }
+
+    @Override
+    public int bindLayout() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    public void initView(Bundle savedInstanceState, View contentView) {
+        et_account = contentView.findViewById(R.id.et_account);
+        et_password = contentView.findViewById(R.id.et_password);
+        tv_get_code = contentView.findViewById(R.id.tv_get_code);
+        btn_login = contentView.findViewById(R.id.btn_login);
+        tv_login_type = contentView.findViewById(R.id.tv_login_type);
+        tv_register = contentView.findViewById(R.id.tv_register);
+        tv_get_code.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void initListener() {
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                refreshSignBtnState();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        };
+        et_account.addTextChangedListener(textWatcher);
+        et_password.addTextChangedListener(textWatcher);
+        tv_get_code.setOnClickListener(this);
+        btn_login.setOnClickListener(this);
+        tv_login_type.setOnClickListener(this);
+        tv_register.setOnClickListener(this);
+    }
+
+    @Override
+    public void doBusiness() {
+        refreshSignBtnState();
+    }
+
+    @Override
+    public void onWidgetClick(View view) {
+        if (view.getId() == R.id.btn_login) {
+
+        }
+        if (view.getId() == R.id.tv_login_type) {
+            RouteUtils.startActivity(RoutePathConfig.SIGNIN_LOGIN_BY_CODE_ACTIVITY);
+        } else if (view.getId() == R.id.tv_register) {
+
+        }
+    }
+
+    protected void refreshSignBtnState() {
+        if (TextUtils.isEmpty(et_account.getText()) || TextUtils.isEmpty(et_password.getText())) {
+            btn_login.setEnabled(false);
+        } else {
+            btn_login.setEnabled(true);
+        }
+    }
+
+    @Override
+    public void onLoginSuccess() {
+        RouteUtils.startActivity(RoutePathConfig.App_Main);
+        finish();
+    }
+
+    @Override
+    public void onLoginFail(String errorMsg) {
+        YXToastUtil.showToast(errorMsg);
+    }
+
+}
---
+++ module_signin/src/main/java/com/yanxiu/gphone/jiaoyan/module/signin/presenter/LoginPresenter.java
@@ -0,0 +1,101 @@
+package com.yanxiu.gphone.jiaoyan.module.signin.presenter;
+
+import com.yanxiu.gphone.jiaoyan.module.signin.interfaces.LoginContract;
+import com.yanxiu.lib.yx_basic_library.base.basemvp.YXBasePresenterImpl;
+
+/**
+ * 登录逻辑处理
+ * Created by Hu Chao on 18/10/10.
+ */
+public class LoginPresenter extends YXBasePresenterImpl<LoginContract.IView> implements LoginContract.IPresenter {
+    public LoginPresenter(LoginContract.IView view) {
+        super(view);
+    }
+
+    @Override
+    public void login(String username, String password) {
+        mView.showLoading();
+        signIn(username, password);
+    }
+
+    /**
+     * 1.请求token
+     *
+     * @param username
+     * @param password
+     */
+    private void signIn(String username, String password) {
+//        mLoginName = username;
+//        SignInRequest request = new SignInRequest();
+//        request.loginName = username;
+//        request.password = YXCryptoDecryptUtil.md5(password);
+//        addRequest(request, SignInResponse.class, new IYXHttpCallback<SignInResponse>() {
+//            @Override
+//            public void onRequestCreated(Request request) {
+//
+//            }
+//
+//            @Override
+//            public void onSuccess(YXRequestBase request, SignInResponse ret) {
+//                if (ret.getCode() == 0) {
+//                    //save token;
+//                    UserInfoManager.getInstance().setToken(ret.getToken());
+//                    UserInfoManager.getInstance().setPassPort(ret.getPassport());
+//                    requestClassData();
+//                } else {
+//                    showLoginFial(getErrorMsgFromResponse(ret));
+//                }
+//            }
+//
+//            @Override
+//            public void onFail(YXRequestBase request, Error error) {
+//                showLoginFial(error.getMessage());
+//            }
+//        });
+    }
+
+
+    /**
+     * 3.获取用户信息
+     */
+    private void getUserInfo() {
+    }
+
+    /**
+     * 登录完成，执行登录完成之后的逻辑
+     */
+    private void checkLoginComplete() {
+//        mView.hideLoading();
+//        //处理 im 信息不全时的登录
+//        if (!ImConfig.isHasInitialazed()) {
+//            showLoginFial(null);
+//            return;
+//        }
+//        PushManager.getInstance().bindAlias(FaceShowApplication.getInstance(), String.valueOf(UserInfoManager.getInstance().getUserInfo().getUserId()));
+//        //开启mqtt
+//        UserInfoManager.getInstance().setLastAccount(mLoginName);
+//        mView.onLoginSuccess();
+//        appUsedDetailRequest();
+    }
+
+
+//    private String getErrorMsgFromResponse(FaceShowBaseResponse ret) {
+//        if (ret != null && ret.getError() != null) {
+//            return ret.getError().getMessage();
+//        } else {
+//            return null;
+//        }
+//    }
+
+    private void showLoginFial(String errorMsg) {
+//        mView.hideLoading();
+//        UserInfoManager.getInstance().clearLoginInfo();
+//        if (!TextUtils.isEmpty(errorMsg)) {
+//            mView.onLoginFail(errorMsg);
+//        } else {
+//            mView.onLoginFail("登录失败");
+//        }
+    }
+
+
+}
