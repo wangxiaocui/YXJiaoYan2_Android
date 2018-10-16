b183d0026da69973a7c83e1b6d989957155fb2e5
huchao <huchao@yanxiu.com>
2018-10-16 11:28:20 +0800

    首页列表布局

--- app/src/main/java/com/yanxiu/gphone/jiaoyan/business/course/adapter/CourseListAdapter.java
+++ app/src/main/java/com/yanxiu/gphone/jiaoyan/business/course/adapter/CourseListAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.test.yanxiu.common_base.base.ui.recycler_view.BaseAdapter;
 import com.test.yanxiu.common_base.base.ui.recycler_view.BaseViewHolder;
@@ -12,10 +11,13 @@ import com.yanxiu.gphone.jiaoyan.R;
 import com.yanxiu.gphone.jiaoyan.business.course.bean.ClassStudyScoreRankingBean;
 
 import java.util.List;
-
+import java.util.Random;
 
 
 public class CourseListAdapter extends BaseAdapter<ClassStudyScoreRankingBean> {
+
+    public static final int COURSE_SINGLE = 0;
+    public static final int COURSE_MULTIPLE = 1;
 
     public CourseListAdapter(Context context) {
         super(context);
@@ -27,8 +29,29 @@ public class CourseListAdapter extends BaseAdapter<ClassStudyScoreRankingBean> {
     }
 
     @Override
+    public int getItemViewType(int position) {
+        int random = new Random().nextInt();
+        if (position < 2) {
+            return COURSE_MULTIPLE;
+        } else if (position < 5) {
+            return COURSE_SINGLE;
+        } else if (position == 5) {
+            return COURSE_MULTIPLE;
+        } else if (random % 4 == 0) {
+            return COURSE_MULTIPLE;
+        } else {
+            return COURSE_SINGLE;
+        }
+    }
+
+    @Override
     public BaseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(mContext).inflate(R.layout.custom_flow_item_layout, parent, false);
+        View view;
+        if (viewType == COURSE_SINGLE) {
+            view = LayoutInflater.from(mContext).inflate(R.layout.course_item_single, parent, false);
+        } else {
+            view = LayoutInflater.from(mContext).inflate(R.layout.course_item_multiple, parent, false);
+        }
         ViewHolder viewHolder = new ViewHolder(mContext, view);
         return viewHolder;
     }
@@ -39,16 +62,13 @@ public class CourseListAdapter extends BaseAdapter<ClassStudyScoreRankingBean> {
     }
 
     class ViewHolder extends BaseViewHolder<ClassStudyScoreRankingBean> {
-        TextView tv_text;
 
         public ViewHolder(Context context, View itemView) {
             super(context, itemView);
-            tv_text = (TextView) itemView;
         }
 
         @Override
         public void setData(final int position, final ClassStudyScoreRankingBean data) {
-            tv_text.setText(data.getUserName());
         }
     }
 
